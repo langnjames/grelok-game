f8478a965f7206eadd0b288f0710d619a95f9909
James L <lang.n.james@gmail.com>
2023-03-26 20:11:38 -0500

    Added comments

--- engine/Text.js
+++ engine/Text.js
@@ -1,12 +1,22 @@
+/**
+ * Engine-level text component
+ */
 class Text extends Component {
+    /** The name of the text component */
     name = "Text"
-
+    /** The color of the text component */
     fillStyle
-
+    /** The string for the text component */
     string 
-
+    /** The font to be used for the text component */
     font
-
+    /**
+     * Creates a text component
+     * Used for adding text to the gameObject
+     * @param {String} string text that will be written out
+     * @param {Color} fillStyle color that it will be displayed with, default: white
+     * @param {String} font what font will be used, defaults to 20px arial
+     */
     constructor(string, fillStyle="white", font="20px Arial"){
         super()
         this.fillStyle = fillStyle
@@ -14,11 +24,17 @@ class Text extends Component {
         this.font = font
     }
 
+    /**
+     * Draw the text the the given canvas context
+     * @param {2DContext} ctx 
+     */
     draw(ctx){
+        /** Sets all parameters for the text and draws it */
         ctx.fillStyle = this.fillStyle
         ctx.font = this.font
         ctx.fillText(this.string, this.transform.x, this.transform.y)
     }
 }
 
+/** This adds Text to the global namespace to be called in different areas */
 window.Text = Text
