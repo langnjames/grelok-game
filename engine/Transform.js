f4c7e7870e7059da0b7f2c4a61e567d4783dd299
James L <lang.n.james@gmail.com>
2023-03-26 20:02:31 -0500

    Added comments!

--- engine/Transform.js
+++ engine/Transform.js
@@ -8,17 +8,33 @@
  * are attached to a game object, only the one and index 0 will
  * be used. It is best practice not to have multiple Transform instances
  * on a game objects.
+ * 
+ * TLDR, each GameObject has one single transform and stores positional information
+ * If something needs to have a different transform, it should be a different Game Object
  */
-
 class Transform extends Component {
+  /** Name of the component, defaults to transform */
   name = "Transform"
+  /** X pos of the transform, default is 0 */
   x = 0
+  /** Y pos of the transform, default is 0 */
   y = 0
+  /** X axis scale of the transform, default is 1 */
   sx = 1
+  /** Y axis scale of the transform, default is 1 */
   sy = 1
+  /** Rotation value of the transform, default is 0 */
   r = 0
 
-
+  /**
+   * This function is used mostly to help lines
+   * Sets x, y, scale, and rotate for a start and end location
+   * @param {Number} startX X's starting location for new transform
+   * @param {Number} startY Y's starting location for new transform
+   * @param {Number} endX X's ending location for new transform
+   * @param {Number} endY Y's ending location for new transform
+   * @returns returns the transform
+   */
   static fromTo(startX, startY, endX, endY) {
     let t = new Transform()
     t.x = (startX + endX) / 2
@@ -32,4 +48,5 @@ class Transform extends Component {
   }
 }
 
+/** This adds transform to the global namespace to be called in different areas */
 window.Transform = Transform;
